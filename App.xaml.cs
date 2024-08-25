095ea88cb68d2659e2c81d3ccfeaad9dd1c6491a
apollw <apollw@gmail.com>
2024-08-25 10:18:35 -0300

    init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AgileFlowMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AgileFlowMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
